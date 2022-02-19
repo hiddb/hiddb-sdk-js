b3e95c647ed990d03af72cca7729ada6b9f136e9
Marc Erdmann <marcerdmann.privat@gmail.com>
2022-02-19 17:10:37 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,26 @@ import { paths } from "./hiddb";
 
 let accessToken: string;
 
-// Make sure that the current accessToken is always included in requests
-axios.interceptors.request.use(
-  function (config) {
-    const bearerToken =  `Bearer ${accessToken}`;
-    console.log(bearerToken);
-    return {
-      ...config,
-      headers: accessToken ? {
-        ...(config.headers ?? {}),
-        Authorization: bearerToken,
-      }: config.headers,
-    };
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
-);
-
 const client = axios.create({
   baseURL: "https://api.hiddb.io/",
   timeout: 30000,
 });
+
+// Make sure that the current accessToken is always included in requests
+client.interceptors.request.use(
+    function (config) {
+      return {
+        ...config,
+        headers: accessToken ? {
+          ...(config.headers ?? {}),
+          Authorization: `Bearer ${accessToken}`,
+        }: config.headers,
+      };
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
 
 export async function userLogin(email: string, password: string) {
   const path = "/user/login" as const;
